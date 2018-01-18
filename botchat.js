59c306627216f0a3edd3340158249ae4c6d6ff1b
ankitc1010 <ankit.c1010@gmail.com>
2018-01-19 01:34:37 +0530

    SetFocus removed from card action

--- botchat.js
+++ botchat.js
@@ -17316,20 +17316,22 @@ var HistoryView = (function (_super) {
         _this.scrollToBottom = true;
         // In order to do their cool horizontal scrolling thing, Carousels need to know how wide they can be.
         // So, at startup, we create this mock Carousel activity and measure it.
-        _this.measurableCarousel = function () {
-            // find the largest possible message size by forcing a width larger than the chat itself
-            return React.createElement(WrappedActivity, { ref: function (x) { return _this.carouselActivity = x; }, activity: {
-                    type: 'message',
-                    id: '',
-                    from: { id: '' },
-                    attachmentLayout: 'carousel'
-                }, format: null, fromMe: false, onClickActivity: null, onClickRetry: null, selected: false, showTimestamp: false, chatBotIconUrl: _this.props.chatBotIconUrl, userIconUrl: _this.props.userIconUrl },
-                React.createElement("div", { style: { width: _this.largeWidth } }, "\u00A0"));
-        };
+        _this.measurableCarousel = function () { return (
+        // find the largest possible message size by forcing a width larger than the chat itself
+        React.createElement(WrappedActivity, { ref: function (x) { return (_this.carouselActivity = x); }, activity: {
+                type: 'message',
+                id: '',
+                from: { id: '' },
+                attachmentLayout: 'carousel'
+            }, format: null, fromMe: false, onClickActivity: null, onClickRetry: null, selected: false, showTimestamp: false, chatBotIconUrl: _this.props.chatBotIconUrl, userIconUrl: _this.props.userIconUrl },
+            React.createElement("div", { style: { width: _this.largeWidth } }, "\u00A0"))); };
         return _this;
     }
     HistoryView.prototype.componentWillUpdate = function () {
-        this.scrollToBottom = (Math.abs(this.scrollMe.scrollHeight - this.scrollMe.scrollTop - this.scrollMe.offsetHeight) <= 1);
+        this.scrollToBottom =
+            Math.abs(this.scrollMe.scrollHeight -
+                this.scrollMe.scrollTop -
+                this.scrollMe.offsetHeight) <= 1;
     };
     HistoryView.prototype.componentDidUpdate = function () {
         if (this.props.format.carouselMargin == undefined) {
@@ -17337,7 +17339,8 @@ var HistoryView = (function (_super) {
             // Measure the message padding by subtracting the known large width
             var paddedWidth = measurePaddedWidth(this.carouselActivity.messageDiv) - this.largeWidth;
             // Subtract the padding from the offsetParent's width to get the width of the content
-            var maxContentWidth = this.carouselActivity.messageDiv.offsetParent.offsetWidth - paddedWidth;
+            var maxContentWidth = this.carouselActivity.messageDiv.offsetParent
+                .offsetWidth - paddedWidth;
             // Subtract the content width from the chat width to get the margin.
             // Next time we need to get the content width (on a resize) we can use this margin to get the maximum content width
             var carouselMargin = this.props.size.width - maxContentWidth;
@@ -17355,7 +17358,8 @@ var HistoryView = (function (_super) {
         var lastActivityFromMe = lastActivity && this.props.isFromMe && this.props.isFromMe(lastActivity);
         // Validating if we are at the bottom of the list or the last activity was triggered by the user.
         if (this.scrollToBottom || lastActivityFromMe) {
-            this.scrollMe.scrollTop = this.scrollMe.scrollHeight - this.scrollMe.offsetHeight;
+            this.scrollMe.scrollTop =
+                this.scrollMe.scrollHeight - this.scrollMe.offsetHeight;
         }
     };
     // At startup we do three render passes:
@@ -17363,13 +17367,12 @@ var HistoryView = (function (_super) {
     // 2. To determine the margins of any given carousel (we just render one mock activity so that we can measure it)
     // 3. (this is also the normal re-render case) To render without the mock activity
     HistoryView.prototype.doCardAction = function (type, value) {
-        this.props.setFocus();
         this.props.onClickCardAction();
         return this.props.doCardAction(type, value);
     };
     HistoryView.prototype.render = function () {
         var _this = this;
-        Chat_1.konsole.log("History props", this);
+        Chat_1.konsole.log('History props', this);
         var content;
         if (this.props.size.width !== undefined) {
             if (this.props.format.carouselMargin === undefined) {
@@ -17378,21 +17381,25 @@ var HistoryView = (function (_super) {
                 content = React.createElement(this.measurableCarousel, null);
             }
             else {
-                content = this.props.activities.map(function (activity, index) {
-                    return React.createElement(WrappedActivity, { format: _this.props.format, key: 'message' + index, activity: activity, showTimestamp: index === _this.props.activities.length - 1 || (index + 1 < _this.props.activities.length && suitableInterval(activity, _this.props.activities[index + 1])), selected: _this.props.isSelected(activity), fromMe: _this.props.isFromMe(activity), chatBotIconUrl: _this.props.chatBotIconUrl, userIconUrl: _this.props.userIconUrl, onClickActivity: _this.props.onClickActivity(activity), onClickRetry: function (e) {
-                            // Since this is a click on an anchor, we need to stop it
-                            // from trying to actually follow a (nonexistant) link
-                            e.preventDefault();
-                            e.stopPropagation();
-                            _this.props.onClickRetry(activity);
-                        } },
-                        React.createElement(ActivityView_1.ActivityView, { format: _this.props.format, size: _this.props.size, activity: activity, onCardAction: function (type, value) { return _this.doCardAction(type, value); }, onImageLoad: function () { return _this.autoscroll(); } }));
-                });
+                content = this.props.activities.map(function (activity, index) { return (React.createElement(WrappedActivity, { format: _this.props.format, key: 'message' + index, activity: activity, showTimestamp: index === _this.props.activities.length - 1 ||
+                        (index + 1 < _this.props.activities.length &&
+                            suitableInterval(activity, _this.props.activities[index + 1])), selected: _this.props.isSelected(activity), fromMe: _this.props.isFromMe(activity), chatBotIconUrl: _this.props.chatBotIconUrl, userIconUrl: _this.props.userIconUrl, onClickActivity: _this.props.onClickActivity(activity), onClickRetry: function (e) {
+                        // Since this is a click on an anchor, we need to stop it
+                        // from trying to actually follow a (nonexistant) link
+                        e.preventDefault();
+                        e.stopPropagation();
+                        _this.props.onClickRetry(activity);
+                    } },
+                    React.createElement(ActivityView_1.ActivityView, { format: _this.props.format, size: _this.props.size, activity: activity, onCardAction: function (type, value) {
+                            return _this.doCardAction(type, value);
+                        }, onImageLoad: function () { return _this.autoscroll(); } }))); });
             }
         }
-        return (React.createElement("div", { className: "wc-message-groups", ref: function (div) { return _this.scrollMe = div || _this.scrollMe; } },
-            React.createElement("div", { className: "wc-message-group-content", ref: function (div) { if (div)
-                    _this.scrollContent = div; } }, content)));
+        return (React.createElement("div", { className: "wc-message-groups", ref: function (div) { return (_this.scrollMe = div || _this.scrollMe); } },
+            React.createElement("div", { className: "wc-message-group-content", ref: function (div) {
+                    if (div)
+                        _this.scrollContent = div;
+                } }, content)));
     };
     return HistoryView;
 }(React.Component));
@@ -17408,8 +17415,14 @@ exports.History = react_redux_1.connect(function (state) { return ({
     botConnection: state.connection.botConnection,
     user: state.connection.user
 }); }, {
-    setMeasurements: function (carouselMargin) { return ({ type: 'Set_Measurements', carouselMargin: carouselMargin }); },
-    onClickRetry: function (activity) { return ({ type: 'Send_Message_Retry', clientActivityId: activity.channelData.clientActivityId }); },
+    setMeasurements: function (carouselMargin) { return ({
+        type: 'Set_Measurements',
+        carouselMargin: carouselMargin
+    }); },
+    onClickRetry: function (activity) { return ({
+        type: 'Send_Message_Retry',
+        clientActivityId: activity.channelData.clientActivityId
+    }); },
     onClickCardAction: function () { return ({ type: 'Card_Action_Clicked' }); },
     // only used to create helper functions below
     sendMessage: Chat_1.sendMessage
@@ -17429,13 +17442,18 @@ exports.History = react_redux_1.connect(function (state) { return ({
     // helper functions
     doCardAction: Chat_1.doCardAction(stateProps.botConnection, stateProps.user, stateProps.format.locale, dispatchProps.sendMessage),
     isFromMe: function (activity) { return activity.from.id === stateProps.user.id; },
-    isSelected: function (activity) { return activity === stateProps.selectedActivity; },
-    onClickActivity: function (activity) { return stateProps.connectionSelectedActivity && (function () { return stateProps.connectionSelectedActivity.next({ activity: activity }); }); }
+    isSelected: function (activity) {
+        return activity === stateProps.selectedActivity;
+    },
+    onClickActivity: function (activity) {
+        return stateProps.connectionSelectedActivity &&
+            (function () { return stateProps.connectionSelectedActivity.next({ activity: activity }); });
+    }
 }); })(HistoryView);
 var getComputedStyleValues = function (el, stylePropertyNames) {
     var s = window.getComputedStyle(el);
     var result = {};
-    stylePropertyNames.forEach(function (name) { return result[name] = parseInt(s.getPropertyValue(name)); });
+    stylePropertyNames.forEach(function (name) { return (result[name] = parseInt(s.getPropertyValue(name))); });
     return result;
 };
 var measurePaddedHeight = function (el) {
@@ -17468,30 +17486,31 @@ var WrappedActivity = (function (_super) {
             case null:
                 timeLine = React.createElement("span", null, this.props.format.strings.messageFailed);
                 break;
-            case "retry":
-                timeLine =
-                    React.createElement("span", null,
-                        this.props.format.strings.messageFailed,
-                        ' ',
-                        React.createElement("a", { href: ".", onClick: this.props.onClickRetry }, this.props.format.strings.messageRetry));
+            case 'retry':
+                timeLine = (React.createElement("span", null,
+                    this.props.format.strings.messageFailed,
+                    ' ',
+                    React.createElement("a", { href: ".", onClick: this.props.onClickRetry }, this.props.format.strings.messageRetry)));
                 break;
             default:
                 var sent = void 0;
                 if (this.props.showTimestamp)
-                    sent = this.props.format.strings.timeSent.replace('%1', (new Date(this.props.activity.timestamp)).toLocaleTimeString());
-                timeLine = React.createElement("span", null,
+                    sent = this.props.format.strings.timeSent.replace('%1', new Date(this.props.activity.timestamp).toLocaleTimeString());
+                timeLine = (React.createElement("span", null,
                     this.props.activity.from.name || this.props.activity.from.id,
-                    sent);
+                    sent));
                 break;
         }
         var who = this.props.fromMe ? 'me' : 'bot';
-        var imgUrl = this.props.fromMe ? this.props.userIconUrl : this.props.chatBotIconUrl;
+        var imgUrl = this.props.fromMe
+            ? this.props.userIconUrl
+            : this.props.chatBotIconUrl;
         var wrapperClassName = Chat_1.classList('wc-message-wrapper', this.props.activity.attachmentLayout || 'list', this.props.onClickActivity && 'clickable');
         var contentClassName = Chat_1.classList('wc-message-content', this.props.selected && 'selected');
         return (React.createElement("div", { "data-activity-id": this.props.activity.id, className: wrapperClassName, onClick: this.props.onClickActivity },
             React.createElement("div", { className: 'wc-message-from-' + who + ' custom-img' },
                 React.createElement("img", { src: imgUrl })),
-            React.createElement("div", { className: 'wc-message wc-message-from-' + who, ref: function (div) { return _this.messageDiv = div; } },
+            React.createElement("div", { className: 'wc-message wc-message-from-' + who, ref: function (div) { return (_this.messageDiv = div); } },
                 React.createElement("div", { className: contentClassName },
                     React.createElement("svg", { className: "wc-message-callout" },
                         React.createElement("path", { className: "point-left", d: "m0,6 l6 6 v-12 z" }),
